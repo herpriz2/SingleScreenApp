8d784602182b04263e4e43417bfea11c2cb4e4e2
herpriz <prisohermann@yahoo.fr>
2019-10-06 00:56:42 +0100

    Initial commit

---
+++ app/src/androidTest/java/com/herpriz/singlescreenapp/ExampleInstrumentedTest.java
@@ -0,0 +1,27 @@
+package com.herpriz.singlescreenapp;
+
+import android.content.Context;
+
+import androidx.test.InstrumentationRegistry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(JUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        assertEquals("com.herpriz.singlescreenapp", appContext.getPackageName());
+    }
+}
